d3010534290283d26185ece4c3b25040b5d6c909
thomasmama14 <92804690+thomasmama14@users.noreply.github.com>
2022-06-19 23:27:48 -0400

    working

---
+++ controllers/journalController.js
@@ -0,0 +1,46 @@
+const Journal = require('../models/Journal');
+
+exports.journalEntry = async (req, res) => {
+  const { entries, userId, title, date, body } = req.body;
+
+  const journalEntryies = await journalEntry.findOne({ userId: userId });
+
+  if (!userId) {
+    const newJournal = new User.Journal({
+      entries: [
+        {
+          userId: { type: Schema.Types.ObjectId, ref: 'User' },
+          title: { type: String },
+          date: { type: String },
+          body: { type: String },
+        },
+      ],
+    });
+
+    newUser.Journal.save();
+    return res.json('Journal Entry Saved');
+  }
+
+  console.log('Entry Not Saved');
+  res.json('Try Again');
+};
+
+exports.handleNewJournal = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const newJournal = {
+      date: data.get('date'),
+      title: data.get('title'),
+      body: data.get('body'),
+      // id: data.get('id'),
+    };
+    console.log('newJournal is ', newJournal);
+    await axios
+      .post('/inspired', newJournal)
+      .then((res) => console.log('Journal Saved'));
+  };
+
+exports.handleDelete = (title) => {
+    const newJournals = journals.filter((journal) => journal.title !== title);
+    setJournals(newJournals);
+  };
--- models/User.js
+++ models/User.js
@@ -1,12 +1,12 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    fName: String,
-    lName: String,
-    username: String,
-    email: String,
-    zipcode: Number,
-    password: String
-})
+  fName: String,
+  lName: String,
+  username: String,
+  email: String,
+  zipcode: Number,
+  password: String,
+});
 
-module.exports = mongoose.model("user", userSchema)
+module.exports = mongoose.model('user', userSchema);
